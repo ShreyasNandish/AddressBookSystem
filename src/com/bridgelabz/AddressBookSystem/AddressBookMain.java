a23c115f7be0c9b026fc51795117705617464f20
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-18 15:23:05 +0530

    Modified: changed the program to add the Contact details

--- src/com/bridgelabz/AddressBookSystem/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookSystem/AddressBookMain.java
@@ -3,6 +3,7 @@ package com.bridgelabz.AddressBookSystem;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program ");
-
+        Contact contact=new Contact();
+        contact.setContactDetails();
     }
 }
