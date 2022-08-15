55d9da02fbda1175b34120dbfa42df16e373c6ab
ShreyasNandish <shreyasnandish4@gmail.com>
2022-08-15 17:10:35 +0530

    Refactor the code to check for duplicate entries

--- src/com/bridgelabz/AddressBookSystem/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookSystem/AddressBookMain.java
@@ -4,50 +4,101 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    public static void main(String[] args) {
-        HashMap<String,AddressBook>map = new HashMap<>();
-        System.out.println("Welcome to Address Book");
-        AddressBook addressBook = new AddressBook();
+    static HashMap<String,AddressBook>map = new HashMap<>();
+    static String currentAddressBookName;
+    static String addressBookName;
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+        Scanner scanner = new Scanner(System.in);
+    boolean exit = false;
+        while (!exit) {
+        System.out.println("""
+                    \nEnter options:
+                    1) To add contact
+                    2) To edit Contact
+                    3) To display Contacts
+                    4) To delete contact
+                    5) To add address book or select addressBook
+                    6) To exit""");
+
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                try {
+                    map.get(currentAddressBookName).addContact();
+                } catch (Exception e) {
+                    System.out.println("\nNo AddressBook Found\n");
+                }
+
+                break;
+            case 2:
+                try {
+                    map.get(currentAddressBookName).editDetails();
+                } catch (Exception e) {
+                    System.out.println("\nNo AddressBook Found\n");
+                }
+                break;
+            case 3:
+                try {
+                    map.get(currentAddressBookName).ShowDetails();
+                } catch (Exception e) {
+                    System.out.println("\nNo AddressBook Found\n");
+                }
+                break;
+            case 4:
+                try {
+                    map.get(currentAddressBookName).deleteContact();
+                } catch (Exception e) {
+                    System.out.println("\nNo AddressBook Found\n");
+                }
+                break;
+            case 5:
+                chooseAddressBook();
+                break;
+            case 6:
+                exit = true;
+                break;
+            default:
+                break;
+        }
+    }
+        System.out.println(map);
+}
+
+
+    static void chooseAddressBook() {
+        System.out.println("""
+        Press 1 to add AddressBook
+        Press 2 to select AddressBook""");
         Scanner scanner = new Scanner(System.in);
 
-        boolean exit = true;
-        while (exit) {
-            AddressBook add = new AddressBook();
-            System.out.println("Enter the address book name :");
-            String addressBookName = scanner.nextLine();
-            if(map.containsKey(addressBookName))
-            {
-                System.out.println("Address already exists");
-            }
-            else {
-                map.put(addressBookName,add);
-            }
-            System.out.println("Select option: \n" +
-                    "1.Add Contact \n" +
-                    "2.Edit Contact \n" +
-                    "3.Show Contact \n"+
-                    "4.Delete \n" +
-                    "5.Exit");
-            int option = scanner.nextInt();
-            switch (option) {
-                case 1:
-                    addressBook.contactDetails();
-                    break;
-                case 2:
-                    addressBook.editDetails();
-                    break;
-                case 3:
-                    addressBook.ShowDetails();
-                    break;
-                case 4:
-                    addressBook.deleteContact();
-                    break;
-                case 5:
-                    exit = false;
-                default:
-                    break;
-            }
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter address book name");
+                addressBookName = scanner.next().toLowerCase();
+                if (map.containsKey(addressBookName)) {
+                    System.out.println("\nAddress book already exist\n");
+                    chooseAddressBook();
+                } else {
+                    AddressBook addressBook = new AddressBook();
+                    map.put(addressBookName, addressBook);
+                    currentAddressBookName = addressBookName;
+                }
+
+                break;
+            case 2:
+                System.out.println("Enter address book name");
+                addressBookName = scanner.next().toLowerCase();
+                if (!map.containsKey(addressBookName)) {
+                    System.out.println("\nAddressBook not Found\n");
+                    chooseAddressBook();
+                } else
+                    currentAddressBookName = addressBookName;
+                break;
+            default:
+                break;
         }
     }
     }
--- src/com/bridgelabz/AddressBookSystem/Contact.java
+++ src/com/bridgelabz/AddressBookSystem/Contact.java
@@ -11,6 +11,71 @@ public class Contact {
     long phoneNumber;
     String email;
 
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(double zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Contact()
     {
 
@@ -40,6 +105,12 @@ public class Contact {
                 ", phoneNumber=" + phoneNumber +
                 ", email='" + email + '\'' +
                 "}\n";
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return super.equals(obj);
     }
 }
 
